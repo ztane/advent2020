c942ea1d65cf959631e60b2dfac2c909782e216d
Antti Haapala <antti.haapala@anttipatterns.com>
2020-12-05 16:27:27 +0200

    use scalar() instead of list()[0]

--- days/day05.py
+++ days/day05.py
@@ -6,5 +6,5 @@ d = get_aoc_data(day=5)
 def part1_and_2():
     t = str.maketrans('FLBR', '0011')
     seat_ids = {int(bpass.translate(t), 2) for bpass in d.lines}
-    missing = list(set(interval(min(seat_ids), max(seat_ids))) - seat_ids)[0]
+    missing = scalar(set(interval(min(seat_ids), max(seat_ids))) - seat_ids)
     return max(seat_ids), missing
