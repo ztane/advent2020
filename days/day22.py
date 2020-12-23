7b586c09b07c460d4326167a3d2b9c8a2e2dfd22
Antti Haapala <antti.haapala@anttipatterns.com>
2020-12-23 08:13:46 +0200

    days 22 and 23

--- days/day22.py
+++ days/day22.py
@@ -1,18 +1,133 @@
 from helpers import *
 
 test_data = Data("""
+Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10
 """)
 
-test_case(1, test_data, ...)
-test_case(2, test_data, ...)
+test_case2 = """
+Player 1:
+43
+19
+
+Player 2:
+2
+29
+14
+"""
+
+test_case(1, test_data, 306)
+test_case(2, test_data, 291)
+test_case(2, test_case2, 105)
 
 
 def part1(d: Data, ans: Answers) -> None:
-    ...
+    players = d.split('\n\n')
+    player_list = {}
+    for i, p in enumerate(players):
+        player_list[i] = deque(p.extract_ints[1:])
+
+    while player_list[0] and player_list[1]:
+        first = player_list[0].popleft()
+        second = player_list[1].popleft()
+        if first > second:
+            player_list[0].append(first)
+            player_list[0].append(second)
+        else:
+            player_list[1].append(second)
+            player_list[1].append(first)
+
+    winning = player_list[0] or player_list[1]
+    print(player_list)
+    rv = 0
+    for i, v in zip(range(len(winning), 0, -1), winning):
+        rv += i * v
+    ans.part1 = rv
 
 
 def part2(d: Data, ans: Answers) -> None:
-    ...
+    next_gameno = 1
+
+    def play_game(p1, p2):
+        nonlocal next_gameno
+        gameno = next_gameno
+        print(f'=== Game {gameno} ===\n')
+        next_gameno += 1
+
+        round = 0
+        previous_rounds = set()
+        while p1 and p2:
+            round += 1
+            print(f'-- Round {round} (Game {gameno}) --')
+            print(f"Player 1's deck: {', '.join(map(str, p1))}")
+            print(f"Player 2's deck: {', '.join(map(str, p2))}")
+
+            if (x := tuple(p1)) in previous_rounds:
+                p2 = []
+                break
+
+            previous_rounds.add(x)
+            c1 = p1.popleft()
+            c2 = p2.popleft()
+
+            print(f"Player 1 plays: {c1}")
+            print(f"Player 2 plays: {c2}")
+
+            if not (len(p1) >= c1 and len(p2) >= c2):
+                if c1 > c2:
+                    winner = 1
+                else:
+                    winner = 2
+
+                print(f"Player {winner} wins round {round} of game {gameno}!\n")
+            else:
+                new_p1 = deque(islice(p1, 0, c1))
+                new_p2 = deque(islice(p2, 0, c2))
+                print("Playing a sub-game to determine the winner...\n")
+                winner = play_game(new_p1, new_p2)
+                print(f"...anyway, back to game {gameno}")
+
+            if winner == 1:
+                p1.append(c1)
+                p1.append(c2)
+
+            elif winner == 2:
+                p2.append(c2)
+                p2.append(c1)
+
+            else:
+                raise ValueError("WTF")
+
+        winning = p1 or p2
+        if p1:
+            winner = 1
+        else:
+            winner = 2
+
+        print(f"The winner of game {gameno} is player {winner}!\n")
+        if gameno == 1:
+            ans.part2 = 0
+            for i, v in zip(range(len(winning), 0, -1), winning):
+                ans.part2 += i * v
+        return winner
+
+    players = d.split('\n\n')
+    player_list = {}
+    for i, p in enumerate(players):
+        player_list[i] = deque(p.extract_ints[1:])
+
+    play_game(player_list[0], player_list[1])
 
 
-run([1], day=22, year=2020, submit=True)
+run([2], day=22, year=2020, submit=True)
--- days/day23.py
+++ days/day23.py
@@ -1,18 +1,131 @@
 from helpers import *
 
 test_data = Data("""
+389125467
 """)
 
-test_case(1, test_data, ...)
-test_case(2, test_data, ...)
+test_case(1, test_data, 67384529)
+test_case(2, test_data, 149245887792)
 
 
 def part1(d: Data, ans: Answers) -> None:
-    ...
+    state = deque(map(int, d.data))
+    for i in range(100):
+        current = state[0]
+        state.rotate(-1)
+        cup1 = state.popleft()
+        cup2 = state.popleft()
+        cup3 = state.popleft()
+
+        cups = [cup1, cup2, cup3]
+        destination = (current - 2) % 9 + 1
+        while destination in cups:
+            destination = (destination - 2) % 9 + 1
+
+        #print('current', current, 'destination', destination, 'pick up', cups)
+        #print('state before rotate', state)
+        location = state.index(destination)
+        state.rotate(-location-1)
+        # print('rotated', location, location - 1)
+        # print('presumed', destination, 'on left', state)
+        state.appendleft(cup3)
+        state.appendleft(cup2)
+        state.appendleft(cup1)
+        location = state.index(current)
+        state.rotate(-location)
+        state.rotate(-1)
+       # print(state)
+
+    state.rotate(i)
+    state.rotate(-state.index(1))
+    state.popleft()
+    ans.part1 = ''.join(map(str, state))
+
+
+class CupNode:
+    next: 'CupNode' = None
+    prev: 'CupNode' = None
+
+    def __init__(self, num):
+        self.num = num
+
+    def unlink(self):
+        self.next.prev = self.prev
+        self.prev.next = self.next
+
+    def link(self, after):
+        self.next = after.next
+        self.prev = after
+        self.next.prev = self
+        after.next = self
+
+    def __str__(self):
+        return f'C{self.num}'
 
 
 def part2(d: Data, ans: Answers) -> None:
-    ...
+    start: Optional[CupNode] = None
+    prev: Optional[CupNode] = None
+
+    node_map: Dict[int, CupNode] = {}
+    max_cup = 0
+    for i in list(map(int, d.data)) + list(interval(10, 1_000_000)):
+        node = CupNode(i)
+        node.prev = prev
+        if prev:
+            prev.next = node
+
+        prev = node
+        if start is None:
+            start = node
+
+        node_map[i] = node
+        max_cup = max(i, max_cup)
+
+    start.prev = node
+    node.next = start
+    current = start
+    for i in range(10_000_000):
+        cup1 = current.next
+        cup1.unlink()
+        cup2 = current.next
+        cup2.unlink()
+        cup3 = current.next
+        cup3.unlink()
+        target = current.num - 1
+        if target == 0:
+            target = max_cup
+
+        if i % 100000 == 0:
+            print(i)
+
+        while target in [cup1.num, cup2.num, cup3.num]:
+            target -= 1
+            if target == 0:
+                target = max_cup
+
+        current = current.next
+        target_node = node_map[target]
+        cup3.link(target_node)
+        cup2.link(target_node)
+        cup1.link(target_node)
 
 
-run([1], day=23, year=2020, submit=True)
+        """
+        c = current
+        while True:
+            print(c, end=' ')
+            c = c.next
+            if c == current:
+                break
+        print()
+"""
+
+    # print(state)
+
+    one = node_map[1]
+
+    ans.part2 = one.next.num * one.next.next.num
+
+
+run([2], day=23, year=2020, submit=True)
